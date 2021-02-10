058604a498a75ebd84f811e69c4a5cd2a595f2d5
zaakir hussain <zaakir@gmail.com>
2021-02-10 20:10:49 +0530

    Activation Fix

--- resto_us/api/transactions/views.py
+++ resto_us/api/transactions/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets,generics
 from .serializers import transactionSerializer,transactionsListSerializer,DealSubscriptionSerializer
 from .models import transaction,UserDealSubscription
 from api.restraunt.models import restraunt,restraunt_standards
+from api.user.models import resto_user
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -33,17 +34,17 @@ class userDealAPI(generics.RetrieveAPIView):
 
 @csrf_exempt
 def activateDeal(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"error":"Not Logged In"})
     if not request.method == 'POST':
         return JsonResponse({"error":"Method "+request.method + " Not allowed."})
     code = request.POST["code"]
     deal_id = request.POST["deal_id"]
+    user_id = request.POST["user_id"]
     try:
         deal = restraunt_standards.objects.get(id=deal_id)
         resto = restraunt.objects.get(id=deal.restraunt_id.id)
+        user = resto_user.objects.get(id=user_id)
         if resto.rest_code == code:
-            UserDealSubscription.objects.create(users_id=request.user,subscription=deal,meals_remaining=deal.no_of_meals)
+            UserDealSubscription.objects.create(users_id=user,subscription=deal,meals_remaining=deal.no_of_meals)
             return JsonResponse({"success":"Deal Subscription Activated"})
         else:
             return JsonResponse({"error": "Invalid Restraunt Code."})
